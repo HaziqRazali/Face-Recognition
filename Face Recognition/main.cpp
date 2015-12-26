629c2c99f99845d911ec4281d665cb04d03207c8
HaziqRazali <ha_haziq_91@hotmail.com>
2015-12-26 21:49:03 +0800

    Epic !

---
+++ Face Recognition/main.cpp
@@ -0,0 +1,124 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+// Function Headers
+void detectAndDisplay(Mat frame);
+string type2str(int type);
+
+// Global variables
+String face_cascade_name = "haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+string window_name = "Capture - Face detection";
+RNG rng(12345);
+
+int main(int argc, const char** argv)
+{
+	VideoCapture capture;
+	Mat frame;
+	//-- 1. Load the cascades
+	if (!face_cascade.load(face_cascade_name)){ printf("--(!)Error loading\n"); return -1; };
+	if (!eyes_cascade.load(eyes_cascade_name)){ printf("--(!)Error loading\n"); return -1; };
+
+	namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+
+	//-- 2. Read the video stream
+	if (capture.open(0))
+	{
+		while (true)
+		{
+
+			capture >> frame;
+
+			//-- 3. Apply the classifier to the frame
+			if (!frame.empty())
+			{
+				detectAndDisplay(frame);
+			}
+
+			else
+			{
+				printf(" --(!) No captured frame -- Break!"); break;
+			}
+
+		}
+	}
+
+	else
+	{
+		std::cerr << "ERROR: Could not open camera" << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay(Mat frame)
+{
+	std::vector<Rect> faces;
+	Mat frame_gray;
+
+	cvtColor(frame, frame_gray, CV_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	//-- Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
+		//ellipse(frame, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+		rectangle(frame, Rect(faces[i].tl(), faces[i].br()), (0, 0, 0), 3);
+
+		Mat faceROI = frame_gray(faces[i]);
+		std::vector<Rect> eyes;
+
+		// In each face, detect eyes
+		eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+
+		for (size_t j = 0; j < eyes.size(); j++)
+		{
+			Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
+			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+			circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
+		}
+	}
+	// Show what you got
+	imshow(window_name, frame);
+	waitKey(1);
+}
+
+string type2str(int type) {
+	string r;
+
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+	switch (depth) {
+	case CV_8U:  r = "8U"; break;
+	case CV_8S:  r = "8S"; break;
+	case CV_16U: r = "16U"; break;
+	case CV_16S: r = "16S"; break;
+	case CV_32S: r = "32S"; break;
+	case CV_32F: r = "32F"; break;
+	case CV_64F: r = "64F"; break;
+	default:     r = "User"; break;
+	}
+
+	r += "C";
+	r += (chans + '0');
+
+	return r;
+}
+
+/*string ty = type2str(frame.type());
+printf("Matrix: %s %dx%d \n", ty.c_str(), frame.cols, frame.rows);*/
