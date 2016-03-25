3033ac831c36a9ba652c3580a60e9909237a66b1
HaziqRazali <ha_haziq_91@hotmail.com>
2016-03-25 19:40:03 +0800

    Pro Draft 1

--- Face Recognition/main.cpp
+++ Face Recognition/main.cpp
@@ -4,11 +4,11 @@
 [] Clean
 [] Get .exe
 
-Others
-Wrapper Function to project data onto Eigenspace
-Wrapper Function to convert file to 32FC1
-Wrapper Function for imshow
-Standardize (row,col) of images
+Impt
+- Reshape: OpenCV traverses from top left to bottom right (so transpose image before reshaping)
+
+Check
+-- All Eigenvectors correct
 
 */
 
@@ -23,7 +23,8 @@ Standardize (row,col) of images
 #include <iostream>
 #include <fstream>
 
-#include "Settings.h"
+#include "settings.h"
+#include "PCA.h"
 #include "time.h"
 #include "omp.h"
 
@@ -37,6 +38,7 @@ public:
 	Mat Image;
 	Mat Image_compressed;
 	String Name;
+	int Label;
 
 	database(Mat _image, String _name) : Image(_image), Name(_name) {}
 
@@ -51,10 +53,11 @@ void updateDisplay(Mat& frame, const vector<Rect>& haarRect, const vector<int>& 
 void detectFaces(Mat& frame, vector<Mat> &haarFaces, vector<Rect>& haarRect);
 
 // Recognition
-void PrincipalComponentsAnalysis(vector<database>& Face, int principalComponents, Mat& eigenFace, Mat& meanFace);
+void PrincipalComponentsAnalysis(vector<database>& Face, int principalComponents, Mat& eigenFace, Mat& u);
 void projectToEigenspace(vector<Mat>& input);
 void projectToEigenspace(vector<database>& face);
-void getMatches(const vector<database>& Faces, const vector<Mat>& haarFaces_compressed, vector<int>& matchID, vector<pair<double, double>>& distanceToBestMatch);
+void getMatches(const vector<database>& Faces, vector<Mat>& haarFaces_compressed, vector<int>& matchID, vector<pair<double, double>>& distanceToBestMatch);
+void mahalanobis(Mat& input);
 
 // Extra unsused functions
 string type2str(int type);
@@ -65,7 +68,7 @@ CascadeClassifier face_cascade;
 string window_name = "Capture - Face detection";
 RNG rng(12345);
 
-Mat eigenFace, meanFace;
+Mat eigenFace, Mean, Eigenvalues;
 Mat display_color = Mat(Size(1920, 1080), CV_8UC3, Scalar(150, 100, 0));
 
 bool viola_called = false;
@@ -84,7 +87,6 @@ int main(int argc, const char** argv)
 	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading\n"); return -1; };
 
 	// Initialize openmp
-	omp_set_num_threads(3);
 
 	// STORE OFFLINE
 	vector<Mat> rotationMatrix;
@@ -98,74 +100,79 @@ int main(int argc, const char** argv)
 	**********************************************************************************/
 
 	// Read all in folder
-	String folder = "Database";
+	String folder = "DatabaseTest";
 	vector<String> files;
 	glob(folder, files);
 
 	// Read in Training Data
 	vector<database> Face;
 
-	string line;
-	ifstream myfile("database.txt");
+	//string line;
+	//ifstream myfile("databasetest.txt");
 
-	int currentCount = 0;
+	//int currentCount = 0;
+	//int Label = 0;
 
-	while (!myfile.eof())
-	{
-		// read line
-		getline(myfile, line);
+	//while (!myfile.eof())
+	//{
+	//	// read line
+	//	getline(myfile, line);
 
-		// Make sure its not empty
-		if (line.empty()) continue;
+	//	// Make sure its not empty
+	//	if (line.empty()) continue;
 
-		// Store as name
-		string name = line.substr(6);
+	//	// Store as name
+	//	string name = line.substr(6);
 
-		// read size of class
-		getline(myfile, line);
-		string size = line.substr(6);
-		istringstream iss(size);
-		int classSize;
-		iss >> classSize;
+	//	// read size of class
+	//	getline(myfile, line);
+	//	string size = line.substr(6);
+	//	istringstream iss(size);
+	//	int classSize;
+	//	iss >> classSize;
 
-		// Store training data
-		for (int i = currentCount; i < currentCount + classSize; ++i)
-		{
-			Mat src = imread(files[i], CV_LOAD_IMAGE_GRAYSCALE);
-			resize(src, src, Size(92, 112));
-			src.convertTo(src, CV_32FC1);
-			Face.push_back(database(src, name));
-		}
+	//	// Store training data
+	//	for (int i = currentCount; i < currentCount + classSize; i++)
+	//	{
+	//		Mat src = imread(files[i], CV_LOAD_IMAGE_GRAYSCALE);
+	//		resize(src, src, Size(92, 112));
+	//		src.convertTo(src, CV_32FC1);
+	//		Face.push_back(database(src, name));
+	//	}
 
-		currentCount = currentCount + classSize;
-	}
+	//	currentCount = currentCount + classSize;
+	//	Label++;
+	//}
+
+	_PrincipalComponentsAnalysis PCA;
+	PCA.train("databasetest.txt",50);
+	
+	cin.ignore();
 
 	/*********************************************************************************
 									Train
 	**********************************************************************************/
 
 	// PCA
-	PrincipalComponentsAnalysis(Face, 50, eigenFace, meanFace);
+	//PrincipalComponentsAnalysis(Face, 50, eigenFace, Mean);
 
 	// Project to Eigenspace
-	projectToEigenspace(Face);
+	//projectToEigenspace(Face);
 
 	/*********************************************************************************
 									BEGIN
 	**********************************************************************************/
 		
 	Mat frame;
-	int threadReady = 0;
-	vector<Rect> All;
 
 	// Initialize camera
 	capture.open(0);
-
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
 
 	// Begin multi thread
-    #pragma omp parallel shared(frame, threadReady)
+	omp_set_num_threads(3);
+    #pragma omp parallel shared(frame)
 	{
 		// Get thread ID
 		int TID = omp_get_thread_num();			
@@ -186,116 +193,126 @@ int main(int argc, const char** argv)
 			// Run default detector on thread 0
 			if (TID == 0)
 			{					
-				if (viola_called)
+				if (true)
 				{
-					/********************************************************
-											DETECTION
-					*********************************************************/
+					/*vector<Mat> test;
+					Mat ttest;
+					Face[11].Image.convertTo(ttest, CV_8UC1);
+					test.push_back(Face[11].Image);
+					imshow("O", ttest);*/
+					detectFaces(frame, haarFaces, haarRect);					// Detect faces
+					for (int i = 0; i < haarFaces.size(); i++)
+					{
+						char windname[40];
+						sprintf(windname, "haha %d", i);
+						namedWindow(windname, CV_WINDOW_NORMAL);
+						imshow(windname, haarFaces[i]);
+					}
 
-					// Detect faces
-					detectFaces(frame, haarFaces, haarRect);
+					projectToEigenspace(haarFaces);								    // Project to Eigenspace
+					//mahalanobis(test[0]);
+					//getMatches(Face, haarFaces, matchID, distanceToBestMatch);	// Get ID of best matches		
 
-					/********************************************************
-											RECOGNITION
-					*********************************************************/
-						
-					// Project to Eigenspace
-					projectToEigenspace(haarFaces);
-
-					// Get ID of best matches
-					getMatches(Face, haarFaces, matchID, distanceToBestMatch);
-						
-					// Draw result on frame
-					//updateDisplay(frame, haarRect, matchID, distanceToBestMatch, Face);
-					viola_called = true;
-					threadReady++;
 				}
-
-				/*frame.copyTo(display_color(input_video));
-				imshow(window_name, display_color);
-				waitKey(1);*/
 			}
 
-			// Run brute force detector on remaining threads
-			if (TID < 3 && TID > 0)
+			// Rotate images and run detector on remaining threads
+			//if (TID == 1 || TID == 2)
+			//{
+			//	// Rotate input frame
+			//	Mat rotated;
+			//	warpAffine(frame, rotated, rotationMatrix[TID-1], frame.size(), INTER_CUBIC);
+
+			//	if (true)
+			//	{
+			//		detectFaces(rotated, haarFaces, haarRect);					// Detect faces
+			//		projectToEigenspace(haarFaces);								// Project to Eigenspace
+			//		getMatches(Face, haarFaces, matchID, distanceToBestMatch);	// Get ID of best matches
+			//	}
+			//}
+			
+			// Wait for all threads
+			#pragma omp barrier
+
+			if (TID == 0)
 			{
-				// Rotate input frame
-				Mat rotated;
-				warpAffine(frame, rotated, rotationMatrix[TID-1], frame.size(), INTER_CUBIC);
-
-				if (viola_called)
-				{
-					/********************************************************
-											DETECTION
-					*********************************************************/
-
-					// Detect faces
-					detectFaces(rotated, haarFaces, haarRect);
-
-					/********************************************************
-											RECOGNITION
-					*********************************************************/
-						
-					// Project to Eigenspace
-					projectToEigenspace(haarFaces);
-
-					// Get ID of best matches
-					getMatches(Face, haarFaces, matchID, distanceToBestMatch);
-
-					//updateDisplay(frame, haarRect, matchID, distanceToBestMatch, Face);
-					viola_called = true;
-					threadReady++;
-				}
+				imshow("L", frame);
+				waitKey(1);
 			}
 
+			// Draw bounding boxes
 			#pragma omp critical
-			updateDisplay(frame, haarRect, matchID, distanceToBestMatch, Face);
-
-            #pragma omp single
 			{
-				frame.copyTo(display_color(input_video));
-				imshow(window_name, display_color);
+				updateDisplay(frame, haarRect, matchID, distanceToBestMatch, Face);
+			}
+
+			//#pragma omp flush(frame)
+
+			// Show result -- Why name change = hang ?
+			if (TID == 0)
+			{
+				//frame.copyTo(display_color(input_video));
+				imshow("Result", frame);
 				waitKey(1);
 			}
 		}
 	}
 }
 
-void PrincipalComponentsAnalysis(vector<database>& Face, int principalComponents, Mat& eigenFace, Mat& meanFace) {
+void PrincipalComponentsAnalysis(vector<database>& Face, int principalComponents, Mat& eigenFace, Mat& mean) {
 
 	// Initialize dimension of column vector
 	int faceDimension = Face[0].Image.rows* Face[0].Image.cols;
 
 	// Initialize size of database
-	int sizeOfDatabase = Face.size();
-
-	meanFace = Mat::zeros(Face[0].Image.size(), CV_32FC1);
-	Mat meanshiftedData = Mat::zeros(Size(sizeOfDatabase, faceDimension), CV_32FC1);
+	int N = Face.size();
 
 	// Compute mean
-	for (int i = 0; i < sizeOfDatabase; i++)	meanFace = meanFace + Face[i].Image;
-	meanFace = meanFace / Face.size();
+	mean = Mat::zeros(Face[0].Image.size(), CV_32FC1);
+	for (int i = 0; i < N; i++) mean = mean + Face[i].Image;
+	mean = mean / Face.size();
 
-	// Normalize data and reshape to column vector
-	for (int i = 0; i < sizeOfDatabase; i++)
+	// Centralize data, reshape to column vector, accumulate matrix D
+	Mat D = Mat::zeros(Size(N, faceDimension), CV_32FC1);
+	
+	// D -- correct means subtraction all correct
+	for (int i = 0; i < N; i++)
 	{
-		Face[i].Image_compressed = Face[i].Image - meanFace;
-		Face[i].Image_compressed.reshape(0, faceDimension).copyTo(meanshiftedData.col(i));
+		Face[i].Image_compressed = Face[i].Image - mean;							// Subtract mean
+		Face[i].Image_compressed = Face[i].Image_compressed.t();					// Transpose before reshaping
+		Face[i].Image_compressed = Face[i].Image_compressed.reshape(0, 92 * 112);	
+		Face[i].Image_compressed.copyTo(D.col(i));									// Fill up Matrix D
 	}
 
-	// Compute covariance (small) matrix 
-	Mat small_CovMat = meanshiftedData.t() * meanshiftedData / sizeOfDatabase;
-
-	// Eigendecompose
+	// Compute small covariance matrix
+	Mat S = (D.t() * D) / N;
+	
+	// Decompose - Eigenvectors and values correct
 	Mat eigenvalues, s_eigenvectors, eigenvectors;
-	eigen(small_CovMat, eigenvalues, s_eigenvectors);
-	eigenvectors = meanshiftedData * s_eigenvectors;
+	eigen(S, eigenvalues, s_eigenvectors); // Eigenvectors stored in rows
+	s_eigenvectors = s_eigenvectors.t();   // Transpose so Eigenvectors are stored in columns
+	eigenvectors   = D * s_eigenvectors;   // Sign different compared to Matlab
 
-	// Retain top principal components
+	// Retain top principal components -- Correct
 	Mat eigenvectors_retained = eigenvectors(Rect(0, 0, principalComponents, faceDimension)).clone();
+	Eigenvalues = eigenvalues(Rect(0, 0, 1, principalComponents));
 
-	// Compress database
-	for (int i = 0; i < sizeOfDatabase; i++)	Face[i].Image_compressed = eigenvectors_retained.t() * Face[i].Image_compressed.reshape(0, faceDimension);
+	// Normalize all eigenvectors -- Correct
+	for (int i = 0; i < eigenvectors_retained.cols; i++)
+	{
+		normalize(eigenvectors_retained.col(i), eigenvectors_retained.col(i), 1.0, 0.0, NORM_L2);
+		/*cout << eigenvectors_retained.col(i) << endl;
+		Mat temp;
+		pow(eigenvectors_retained.col(i), 2, temp);
+		cout << sum(temp)(0) << endl;
+		waitKey(0);*/
+	}
+	
+	// Project onto Eigenspace
+	for (int i = 0; i < N; i++)
+	{
+		Face[i].Image_compressed = eigenvectors_retained.t() * Face[i].Image_compressed.reshape(0, faceDimension);
+	}
 
 	eigenFace = eigenvectors_retained;
 }
@@ -306,9 +323,9 @@ void projectToEigenspace(vector<Mat>& input) {
 	for (int i = 0; i < input.size(); i++)
 	{
 		if (type2str(input[i].type()).compare("8UC1") == 0)	input[i].convertTo(input[i], CV_32FC1);
-
 		Mat temp_input = input[i].clone();
-		temp_input = temp_input - meanFace;
+		temp_input = temp_input - Mean;
+		temp_input = temp_input.t();
 		input[i] = eigenFace.t() * temp_input.reshape(0, temp_input.rows * temp_input.cols);
 	}
 }
@@ -321,7 +338,7 @@ void projectToEigenspace(vector<database>& face) {
 		if (type2str(face[i].Image.type()).compare("8UC1") == 0)	face[i].Image.convertTo(face[i].Image, CV_32FC1);
 
 		Mat temp_input = face[i].Image.clone();
-		temp_input = temp_input - meanFace;
+		temp_input = temp_input - Mean;
 		face[i].Image_compressed = eigenFace.t() * temp_input.reshape(0, temp_input.rows * temp_input.cols);
 	}
 }
@@ -342,12 +359,13 @@ void detectFaces(Mat& frame, vector<Mat> &haarFaces, vector<Rect>& haarRect) {
 	// Update haarFaces
 	for (int i = 0; i < _haarRect.size(); i++)
 	{
-		_haarRect[i] = Rect(_haarRect[i].tl().x + 0.1*_haarRect[i].width, _haarRect[i].tl().y + 0.1*_haarRect[i].height, 0.8*_haarRect[i].width, 0.8*_haarRect[i].height);
+		_haarRect[i] = Rect(_haarRect[i].tl().x + 0.17*_haarRect[i].width, _haarRect[i].tl().y + 0.17*_haarRect[i].height, 0.66*_haarRect[i].width, 0.7*_haarRect[i].height);
 
 		Rect roi = _haarRect[i] & Rect(0, 0, 640, 480);
 
 		// Convert to Grayscale and resize before pushing
 		Mat _haarCandidate = frame_gray(roi);
+		equalizeHist(_haarCandidate, _haarCandidate);
 		Mat _colorFace = color(roi);
 
 		resize(_haarCandidate, _haarCandidate, Size(92, 112));
@@ -364,22 +382,28 @@ void updateDisplay(Mat& frame, const vector<Rect>& haarRect, const vector<int>& 
 	for (int i = 0; i < haarRect.size(); i++)
 	{
 		// Convert for display
-		Face[matchID[i]].Image.convertTo(Face[matchID[i]].Image, CV_8U);
+		//Face[matchID[i]].Image.convertTo(Face[matchID[i]].Image, CV_8U);
 
 		// Draw bounding box
 		rectangle(frame, haarRect[i], CV_RGB(0, 0, 0));
-		putText(frame, Face[matchID[i]].Name, haarRect[i].tl(), FONT_HERSHEY_SIMPLEX, 1, CV_RGB(255, 0, 0));
+		//putText(frame, Face[matchID[i]].Name, haarRect[i].tl(), FONT_HERSHEY_SIMPLEX, 1, CV_RGB(255, 0, 0));
 
 		//cout << distanceToBestMatch[i].first << " " << distanceToBestMatch[i].second << endl;
 	}
 
 }
 
-void getMatches(const vector<database>& Faces, const vector<Mat>& haarFaces_compressed, vector<int>& matchID, vector<pair<double, double>>& distanceToBestMatch) {
+void getMatches(const vector<database>& Faces, vector<Mat>& haarFaces_compressed, vector<int>& matchID, vector<pair<double, double>>& distanceToBestMatch) {
+
+	//for (int i = 0; i < haarFaces_compressed.size(); i++)
+	//{
+	//	mahalanobis(haarFaces_compressed[i]);
+	//	cout << endl;
+	//}
 
 	int numberOfCandidates = haarFaces_compressed.size();
 	int numberOfFaces = Faces.size();
-
+	
 	// Loop through Candidates
 	for (int i = 0; i < numberOfCandidates; i++)
 	{
@@ -399,6 +423,24 @@ void getMatches(const vector<database>& Faces, const vector<Mat>& haarFaces_comp
 		distanceToBestMatch.push_back(make_pair(euclideanDist[0], euclideanDist[1]));
 	}
 
+}
+
+void mahalanobis(Mat& input) {
+
+	//cout << input << endl;
+
+	// Square
+	Mat squared = input.clone();
+	pow(input, 2, squared);
+
+	// Compute distance
+	Mat out;
+	divide(squared, Eigenvalues, out);
+
+	//cout << out << endl;
+
+	//cout << "maha " << sum(out) << endl;
+	//waitKey(0);
 }
 
 void initializeDisplay() {
@@ -454,4 +496,4 @@ string type2str(int type) {
 }
 
 /*string ty = type2str(frame.type());
-printf("Matrix: %s %dx%d \n", ty.c_str(), frame.cols, frame.rows);*/
+printf("Matrix: %s %D%d \n", ty.c_str(), frame.cols, frame.rows);*/
